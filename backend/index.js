dd207070c0227a7b1f054e1a21acd438e106de88
PedroHChaves <phedroxaves@hotmail.com>
2021-02-22 14:37:48 -0300

    Diurno ou Noturno completo

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,96 @@ const app = express()
 const cors = require("cors")
 
 var hrs_trabalhadas = 0
+var min_trabalhados = 0
+var verif_diurno = 0
+var verif_noturno = 0
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    /*if(req.params.hr_inicio >= 5){
-        if(req.params.hr_final <= 22){
-            hrs_trabalhadas = req.params.hr_final - req.params.hr_inicio
-            res.send("Horario diurno: " + hrs_trabalhadas)
+app.post("/", (req, res) => {
+    console.log(req.body.inicio)
+
+    var hr_inicio = parseInt(req.body.inicio.substring(0, 2))
+    var min_inicio = parseInt(req.body.inicio.substring(3, 5))
+    var hr_fim = parseInt(req.body.fim.substring(0, 2))
+    var min_fim = parseInt(req.body.fim.substring(3, 5))
+
+    /*console.log(hr_inicio)
+    console.log(min_inicio)
+    console.log(typeof hr_inicio)
+    console.log(typeof min_inicio)    
+
+    console.log(hr_fim)
+    console.log(min_fim)
+    console.log(typeof hr_fim)
+    console.log(typeof min_fim)*/
+
+    if(hr_inicio <= hr_fim){
+        if(hr_inicio >= 5 && hr_inicio < 22){
+            verif_diurno = 1
+    
+            if(hr_fim > 22){
+                verif_noturno = 1
+            }
+        }else if(hr_inicio >= 22){
+            verif_noturno = 1
+        }else{
+            verif_noturno = 1
+
+            if(hr_fim > 5){
+                verif_diurno = 1
+            }
         }
-    }*/
+    }else{
+        if(hr_inicio >= 22){
+            verif_noturno = 1
+
+            if(hr_fim > 5){
+                verif_diurno = 1
+            }
+        }
+    }
+
+    var diurno = verif_diurno
+    var noturno = verif_noturno
+    verif_diurno = 0
+    verif_noturno = 0
+
+    if(noturno == 1 && diurno == 0){
+        if(hr_fim < hr_inicio){
+            hrs_trabalhadas = 24 - hr_inicio + hr_fim
+    
+            if(min_fim < min_inicio){
+                hrs_trabalhadas = hrs_trabalhadas -1
+                min_trabalhados = 60 - min_inicio + min_fim
+            }else{
+                min_trabalhados = min_fim - min_inicio
+            }
+    
+            res.json({
+                message: "Você trabalhou: " + hrs_trabalhadas.toString() + ":" + 
+                min_trabalhados.toString() + " no período Noturno!"
+            })
+        }
+    }else if(diurno == 1 && noturno == 0){
+        if(min_fim < min_inicio){
+            hrs_trabalhadas = hr_fim - hr_inicio -1
+            min_trabalhados = 60 - min_inicio + min_fim
+        }else{
+            hrs_trabalhadas = hr_fim - hr_inicio
+            min_trabalhados = min_fim - min_inicio
+        }
+
+        res.json({
+            message: "Você trabalhou: " + hrs_trabalhadas.toString() + ":" + 
+            min_trabalhados.toString() + " no período Diurno!"
+        })
+    }else{
+        res.json({
+            message: "Manha e Noite"
+        })
+    }
 })
 
 app.listen(3333)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,56 +6,65 @@ import "./sidebar.css"
 
 function App() {
 
-  const [inicio, setInicio] = useState("")
-  const [fim, setFim] = useState("")
+    const [inicio, setInicio] = useState("")
+    const [fim, setFim] = useState("")
 
+    function enviaBack(e){
+        e.preventDefault()
+        let request = {
+            inicio: inicio,
+            fim: fim
+        }
 
-  function enviaBack(e){
-    e.preventDefault()
-    let request = {
-      inicio: inicio,
-      fim: fim
+        //alert(request.inicio + " " + request.fim)
+
+        axios.post("http://localhost:3333/", request).then(resp => {
+            alert(resp.data.message)
+            //console.log("Cheguei")
+        }).catch(err => {
+            console.log(err)
+        })
     }
 
-    /*axios.post("http://localhost:3000/verifica", request).then(resp => {
-      alert(resp.data.message)
-      console.log("Cheguei")
-    }).catch(err => {
-      console.log(err)
-    })*/
-  }
+    return (
+        <div>
 
-  return (
-    <div>
+            <main>
+                <aside>
 
-      <main>
-        <aside>
+                    <strong>Sistema Espediente</strong>
+                    <br />
+                    <form onSubmit={e => enviaBack(e)}>
 
-          <strong>Sistema Espediente</strong>
-          <br />
-          <form onSubmit={e => enviaBack(e)}>
+                        <div className="input-block">
 
-            <div className="input-block">
+                            <label htmlFor="inicio">
+                                Insira o horário de início do espediente
+                            </label>
+                            <br />
+                            <input type="time" value={inicio} onChange={e => setInicio(e.target.value)} required>
+                            </input>
+                            <br /><br />
+                            <label htmlFor="fim">
+                                Insira o horário de fim do espediente
+                            </label>
+                            <br />
+                            <input type="time" value={fim} onChange={e => setFim(e.target.value)} required>
+                            </input>
+                            <br /><br />
+                            <button type="submit">
+                                Confirmar
+                            </button>
 
-              <label htmlFor="inicio">Insira o horário de início do espediente</label>
-              <br />
-              <input type="time" value={inicio} onChange={e => setInicio(e.target.value)} required></input>
-              <br /><br />
-              <label htmlFor="fim">Insira o horário de fim do espediente</label>
-              <br />
-              <input type="time" value={fim} onChange={e => setFim(e.target.value)} required></input>
-              <br /><br />
-              <button type="submit">Confirmar</button>
+                        </div>
 
-            </div>
+                    </form>
 
-          </form>
+                </aside>
+            </main>
 
-        </aside>
-      </main>
-
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
